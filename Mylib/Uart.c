416c8cbfbdf5bb090f225b185f4764e432871f45
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-07-31 13:38:14 +0700

    update uart library

--- Mylib/Uart.c
+++ Mylib/Uart.c
@@ -5,30 +5,35 @@ LV_Embedded
 
 #include "Uart.h"
 
+
+uint8_t aTxBuffer[BUFFERSIZE] = "Vien dep trai ";
+uint8_t aRxBuffer[BUFFERSIZE];
+
 void USART_Config(void)
 {
     /* enable clk gpio and uart */
-    RCC_AHB1PeriphClockCmd(USARTx_CLK, ENABLE);
-    RCC_APB1PeriphClockCmd(USARTx_TX_GPIO_CLK, ENABLE);
-    RCC_APB1PeriphClockCmd(USARTx_RX_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTx_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTx_TX_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTx_RX_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
     /* GPIO Configuration */
     /* TX */
     GPIO_InitTypeDef GPIO_InitStructure;
+
     GPIO_InitStructure.GPIO_Pin = USARTx_TX_PIN;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
     GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStructure);
 
     /* RX */
     GPIO_InitStructure.GPIO_Pin = USARTx_RX_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
     GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStructure);
 
     /* GPIO AF Configuration */
-    GPIO_PinAFConfig(USARTx_TX_GPIO_PORT, USARTx_TX_SOURCE, USARTx_TX_AF);
-    GPIO_PinAFConfig(USARTx_RX_GPIO_PORT, USARTx_RX_SOURCE, USARTx_RX_AF);
+    /*     GPIO_PinAFConfig(USARTx_TX_GPIO_PORT, USARTx_TX_SOURCE, USARTx_TX_AF);
+    GPIO_PinAFConfig(USARTx_RX_GPIO_PORT, USARTx_RX_SOURCE, USARTx_RX_AF); */
 
     /* USART Configuration */
     USART_InitTypeDef USART_InitStructure;
@@ -37,13 +42,27 @@ void USART_Config(void)
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
     /* When using Parity the word length must be configured to 9 bits */
     USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_BaudRate = 115200;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 
     USART_Init(USARTx, &USART_InitStructure);
-}
 
+    /* Interupt Configuration */
+#ifdef _NVIC_
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = USARTx_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    USART_ClearFlag(USARTx, USART_IT_RXNE);
+    USART_ITConfig(USARTx, USART_IT_RXNE, ENABLE);
+#endif
+    USART_Cmd(USARTx, ENABLE);
+}
+#ifdef _DMA_
 void DMAx_Config(void)
 {
     /* Configure DMA Controller to manage USART TX and RX DMA request */
@@ -75,29 +94,31 @@ void DMAx_Config(void)
     DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)aRxBuffer;
     DMA_Init(USARTx_RX_DMA_STREAM, &DMA_InitStructure);
 
-    DMA_Cmd(USARTx_RX_DMA_STREAM,ENABLE);
+    DMA_Cmd(USARTx_RX_DMA_STREAM, ENABLE);
     USART_Cmd(USARTx, ENABLE);
 }
-
+#endif
 
 /* Function Uart TX Rx not use DMA */
 uint16_t USART_Getchar()
 {
     /* waiting for receive data */
-    while (!USART_GetFlagStatus(USART2, USART_FLAG_RXNE));
+    while (!USART_GetFlagStatus(USARTx, USART_FLAG_RXNE))
+        ;
     /* read data */
-    return USART_ReceiveData(USART2);
+    return USART_ReceiveData(USARTx);
 }
 
-void USART_PutChar(char c)
+void USART_PutChar(uint8_t c)
 {
     /* waiting for trans data */
-    while (!USART_GetFlagStatus(USART2, USART_FLAG_TXE));
+    while (!USART_GetFlagStatus(USARTx, USART_FLAG_TXE))
+        ;
     /* Send data */
-    USART_SendData(USART2, c);
+    USART_SendData(USARTx, c);
 }
 
-void USART_PutString(char *s)
+void USART_PutString(uint8_t *s)
 {
     while (*s)
     {
--- Mylib/Uart.h
+++ Mylib/Uart.h
@@ -5,27 +5,28 @@ LV_Embedded
 #ifndef UART_H
 #define UART_H
 
-#include "../Src/stm32f4xx.h"
+#include "stm32f10x.h"
 
 #define BUFFERSIZE              100
 
-uint8_t aTxBuffer[BUFFERSIZE] = "Vien dep trai ";
-uint8_t aRxBuffer[BUFFERSIZE];
+extern uint8_t aTxBuffer[BUFFERSIZE];
+extern uint8_t aRxBuffer[BUFFERSIZE];
 
 /* Difinition for USARTx resources  */
-#define USARTx                  USART2
-#define USARTx_CLK              RCC_APB1Periph_USART2
-#define USARTx_TX_GPIO_CLK      RCC_AHB1Periph_GPIOA
-#define USARTx_TX_PIN           GPIO_Pin_2
+#define USARTx                  USART1
+#define USARTx_IRQn             USART1_IRQn
+#define USARTx_CLK              RCC_APB2Periph_USART1
+#define USARTx_TX_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define USARTx_TX_PIN           GPIO_Pin_9
 #define USARTx_TX_GPIO_PORT     GPIOA
-#define USARTx_TX_SOURCE        GPIO_PinSource2
-#define USARTx_TX_AF            GPIO_AF_USART2
+#define USARTx_TX_SOURCE        GPIO_PinSource9
+//#define USARTx_TX_AF            GPIO_AF_USART2
 
-#define USARTx_RX_GPIO_CLK      RCC_APB1Periph_USART2
-#define USARTx_RX_PIN           GPIO_Pin_3
+#define USARTx_RX_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define USARTx_RX_PIN           GPIO_Pin_10
 #define USARTx_RX_GPIO_PORT     GPIOA
-#define USARTx_RX_SOURCE        GPIO_PinSource3
-#define USARTx_RX_AF            GPIO_AF_USART2
+#define USARTx_RX_SOURCE        GPIO_PinSource10
+//#define USARTx_RX_AF            GPIO_AF_USART2
 
 /* Definition for DMAx resources */
 
@@ -35,7 +36,7 @@ uint8_t aRxBuffer[BUFFERSIZE];
 #define USARTx_DMA                       DMA1
 #define USARTx_DMAx_CLK                  RCC_AHB1Periph_DMA1
      
-#define USARTx_TX_DMA_CHANNEL            DMA_Channel_4
+#define USARTx_TX_DMA_CHANNEL            DMA_Channel2
 #define USARTx_TX_DMA_STREAM             DMA1_Stream6
 #define USARTx_TX_DMA_FLAG_FEIF          DMA_FLAG_FEIF3
 #define USARTx_TX_DMA_FLAG_DMEIF         DMA_FLAG_DMEIF3
@@ -58,9 +59,9 @@ uint8_t aRxBuffer[BUFFERSIZE];
 
 
 
-uint16_t USART_GetChar();
-void USART_PutChar(char c);
-void USART_PutString(char *s);
+uint16_t USART_GetChar(void);
+void USART_PutChar(uint8_t c);
+void USART_PutString(uint8_t *s);
 void USART_Config(void);
 void DMAx_Config(void);
 
