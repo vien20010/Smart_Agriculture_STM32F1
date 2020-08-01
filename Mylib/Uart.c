b9a1d83ed2a2d473198a1161b7453ad8bf539366
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-08-01 15:57:26 +0700

    update all project

--- Mylib/Uart.c
+++ Mylib/Uart.c
@@ -12,28 +12,28 @@ uint8_t aRxBuffer[BUFFERSIZE];
 void USART_Config(void)
 {
     /* enable clk gpio and uart */
-    RCC_APB2PeriphClockCmd(USARTx_CLK, ENABLE);
-    RCC_APB2PeriphClockCmd(USARTx_TX_GPIO_CLK, ENABLE);
-    RCC_APB2PeriphClockCmd(USARTx_RX_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTy_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTy_TX_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(USARTy_RX_GPIO_CLK, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
     /* GPIO Configuration */
     /* TX */
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    GPIO_InitStructure.GPIO_Pin = USARTx_TX_PIN;
+    GPIO_InitStructure.GPIO_Pin = USARTy_TX_PIN;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStructure);
+    GPIO_Init(USARTy_TX_GPIO_PORT, &GPIO_InitStructure);
 
     /* RX */
-    GPIO_InitStructure.GPIO_Pin = USARTx_RX_PIN;
+    GPIO_InitStructure.GPIO_Pin = USARTy_RX_PIN;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStructure);
+    GPIO_Init(USARTy_RX_GPIO_PORT, &GPIO_InitStructure);
 
     /* GPIO AF Configuration */
-    /*     GPIO_PinAFConfig(USARTx_TX_GPIO_PORT, USARTx_TX_SOURCE, USARTx_TX_AF);
-    GPIO_PinAFConfig(USARTx_RX_GPIO_PORT, USARTx_RX_SOURCE, USARTx_RX_AF); */
+    /*     GPIO_PinAFConfig(USARTy_TX_GPIO_PORT, USARTy_TX_SOURCE, USARTy_TX_AF);
+    GPIO_PinAFConfig(USARTy_RX_GPIO_PORT, USARTy_RX_SOURCE, USARTy_RX_AF); */
 
     /* USART Configuration */
     USART_InitTypeDef USART_InitStructure;
@@ -46,21 +46,29 @@ void USART_Config(void)
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 
-    USART_Init(USARTx, &USART_InitStructure);
+    USART_Init(USARTy, &USART_InitStructure);
 
     /* Interupt Configuration */
 #ifdef _NVIC_
     NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = USARTx_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = USARTy_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
-    USART_ClearFlag(USARTx, USART_IT_RXNE);
-    USART_ITConfig(USARTx, USART_IT_RXNE, ENABLE);
+    USART_ClearFlag(USARTy, USART_IT_RXNE);
+    USART_ITConfig(USARTy, USART_IT_RXNE, ENABLE);
 #endif
-    USART_Cmd(USARTx, ENABLE);
+
+    /* RS485 Driver Pin Configuration */
+#ifdef _RS485_
+    RCC_APB2PeriphClockCmd(RS485_DRIVER_CLOCK,ENABLE);
+    Set_Pin_Output(RS485_DRIVER_PORT,RS485_DRIVER_PIN);
+#endif
+
+    /* Enable USARTy */
+    USART_Cmd(USARTy, ENABLE);
 }
 #ifdef _DMA_
 void DMAx_Config(void)
@@ -76,26 +84,26 @@ void DMAx_Config(void)
     DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
     DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(USARTx->DR));
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(USARTy->DR));
     DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
     DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
     DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
     DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 
     /* Configure TX DMA */
-    DMA_InitStructure.DMA_Channel = USARTx_TX_DMA_CHANNEL;
+    DMA_InitStructure.DMA_Channel = USARTy_TX_DMA_CHANNEL;
     DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
     DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)aTxBuffer;
-    DMA_Init(USARTx_TX_DMA_STREAM, &DMA_InitStructure);
+    DMA_Init(USARTy_TX_DMA_STREAM, &DMA_InitStructure);
 
     /* Configure RX DMA */
-    DMA_InitStructure.DMA_Channel = USARTx_RX_DMA_CHANNEL;
+    DMA_InitStructure.DMA_Channel = USARTy_RX_DMA_CHANNEL;
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
     DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)aRxBuffer;
-    DMA_Init(USARTx_RX_DMA_STREAM, &DMA_InitStructure);
+    DMA_Init(USARTy_RX_DMA_STREAM, &DMA_InitStructure);
 
-    DMA_Cmd(USARTx_RX_DMA_STREAM, ENABLE);
-    USART_Cmd(USARTx, ENABLE);
+    DMA_Cmd(USARTy_RX_DMA_STREAM, ENABLE);
+    USART_Cmd(USARTy, ENABLE);
 }
 #endif
 
@@ -103,19 +111,19 @@ void DMAx_Config(void)
 uint16_t USART_Getchar()
 {
     /* waiting for receive data */
-    while (!USART_GetFlagStatus(USARTx, USART_FLAG_RXNE))
+    while (!USART_GetFlagStatus(USARTy, USART_FLAG_RXNE))
         ;
     /* read data */
-    return USART_ReceiveData(USARTx);
+    return USART_ReceiveData(USARTy);
 }
 
 void USART_PutChar(uint8_t c)
 {
     /* waiting for trans data */
-    while (!USART_GetFlagStatus(USARTx, USART_FLAG_TXE))
+    while (!USART_GetFlagStatus(USARTy, USART_FLAG_TXE))
         ;
     /* Send data */
-    USART_SendData(USARTx, c);
+    USART_SendData(USARTy, c);
 }
 
 void USART_PutString(uint8_t *s)
@@ -123,5 +131,25 @@ void USART_PutString(uint8_t *s)
     while (*s)
     {
         USART_PutChar(*s++);
+    }
+}
+
+void RS485_PutChar(USART_TypeDef *USARTx, uint8_t c)
+{
+    while (!USART_GetFlagStatus(USARTx, USART_FLAG_TXE))
+    {
+    }
+    USART_SendData(USARTx, c);
+}
+
+void RS485_PutString(USART_TypeDef *USARTx, uint8_t *s)
+{
+		int i=0;
+    while (*s)
+    {
+        GPIO_SetBits(RS485_DRIVER_PORT,RS485_DRIVER_PIN);
+        RS485_PutChar(USARTx, *s++);
+				i++;
+        GPIO_SetBits(RS485_DRIVER_PORT,RS485_DRIVER_PIN);
     }
 }
--- Mylib/Uart.h
+++ Mylib/Uart.h
@@ -6,56 +6,65 @@ LV_Embedded
 #define UART_H
 
 #include "stm32f10x.h"
+#include "GPIO.h"
 
 #define BUFFERSIZE              100
 
 extern uint8_t aTxBuffer[BUFFERSIZE];
 extern uint8_t aRxBuffer[BUFFERSIZE];
 
-/* Difinition for USARTx resources  */
-#define USARTx                  USART1
-#define USARTx_IRQn             USART1_IRQn
-#define USARTx_CLK              RCC_APB2Periph_USART1
-#define USARTx_TX_GPIO_CLK      RCC_APB2Periph_GPIOA
-#define USARTx_TX_PIN           GPIO_Pin_9
-#define USARTx_TX_GPIO_PORT     GPIOA
-#define USARTx_TX_SOURCE        GPIO_PinSource9
-//#define USARTx_TX_AF            GPIO_AF_USART2
+/* Difinition for USARTy resources  */
+#define USARTy                  USART1
+#define USARTy_IRQn             USART1_IRQn
+#define USARTy_CLK              RCC_APB2Periph_USART1
+#define USARTy_TX_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define USARTy_TX_PIN           GPIO_Pin_9
+#define USARTy_TX_GPIO_PORT     GPIOA
+#define USARTy_TX_SOURCE        GPIO_PinSource9
+//#define USARTy_TX_AF            GPIO_AF_USART2
 
-#define USARTx_RX_GPIO_CLK      RCC_APB2Periph_GPIOA
-#define USARTx_RX_PIN           GPIO_Pin_10
-#define USARTx_RX_GPIO_PORT     GPIOA
-#define USARTx_RX_SOURCE        GPIO_PinSource10
-//#define USARTx_RX_AF            GPIO_AF_USART2
+#define USARTy_RX_GPIO_CLK      RCC_APB2Periph_GPIOA
+#define USARTy_RX_PIN           GPIO_Pin_10
+#define USARTy_RX_GPIO_PORT     GPIOA
+#define USARTy_RX_SOURCE        GPIO_PinSource10
+//#define USARTy_RX_AF            GPIO_AF_USART2
 
 /* Definition for DMAx resources */
+/* Uncomment the line below if u want to usr DMA */
+//#define _DMA_
 
+#define USARTy_DR_ADDRESS                ((uint32_t)USART2 + 0x04) 
 
-#define USARTx_DR_ADDRESS                ((uint32_t)USART2 + 0x04) 
-
-#define USARTx_DMA                       DMA1
-#define USARTx_DMAx_CLK                  RCC_AHB1Periph_DMA1
+#define USARTy_DMA                       DMA1
+#define USARTy_DMAx_CLK                  RCC_AHB1Periph_DMA1
      
-#define USARTx_TX_DMA_CHANNEL            DMA_Channel2
-#define USARTx_TX_DMA_STREAM             DMA1_Stream6
-#define USARTx_TX_DMA_FLAG_FEIF          DMA_FLAG_FEIF3
-#define USARTx_TX_DMA_FLAG_DMEIF         DMA_FLAG_DMEIF3
-#define USARTx_TX_DMA_FLAG_TEIF          DMA_FLAG_TEIF3
-#define USARTx_TX_DMA_FLAG_HTIF          DMA_FLAG_HTIF3
-#define USARTx_TX_DMA_FLAG_TCIF          DMA_FLAG_TCIF3
+#define USARTy_TX_DMA_CHANNEL            DMA_Channel2
+#define USARTy_TX_DMA_STREAM             DMA1_Stream6
+#define USARTy_TX_DMA_FLAG_FEIF          DMA_FLAG_FEIF3
+#define USARTy_TX_DMA_FLAG_DMEIF         DMA_FLAG_DMEIF3
+#define USARTy_TX_DMA_FLAG_TEIF          DMA_FLAG_TEIF3
+#define USARTy_TX_DMA_FLAG_HTIF          DMA_FLAG_HTIF3
+#define USARTy_TX_DMA_FLAG_TCIF          DMA_FLAG_TCIF3
               
-#define USARTx_RX_DMA_CHANNEL            DMA_Channel_4
-#define USARTx_RX_DMA_STREAM             DMA1_Stream5
-#define USARTx_RX_DMA_FLAG_FEIF          DMA_FLAG_FEIF1
-#define USARTx_RX_DMA_FLAG_DMEIF         DMA_FLAG_DMEIF1
-#define USARTx_RX_DMA_FLAG_TEIF          DMA_FLAG_TEIF1
-#define USARTx_RX_DMA_FLAG_HTIF          DMA_FLAG_HTIF1
-#define USARTx_RX_DMA_FLAG_TCIF          DMA_FLAG_TCIF1
+#define USARTy_RX_DMA_CHANNEL            DMA_Channel_4
+#define USARTy_RX_DMA_STREAM             DMA1_Stream5
+#define USARTy_RX_DMA_FLAG_FEIF          DMA_FLAG_FEIF1
+#define USARTy_RX_DMA_FLAG_DMEIF         DMA_FLAG_DMEIF1
+#define USARTy_RX_DMA_FLAG_TEIF          DMA_FLAG_TEIF1
+#define USARTy_RX_DMA_FLAG_HTIF          DMA_FLAG_HTIF1
+#define USARTy_RX_DMA_FLAG_TCIF          DMA_FLAG_TCIF1
 
-#define USARTx_DMA_TX_IRQn               DMA1_Stream3_IRQn
-#define USARTx_DMA_RX_IRQn               DMA1_Stream1_IRQn
-#define USARTx_DMA_TX_IRQHandler         DMA1_Stream3_IRQHandler
-#define USARTx_DMA_RX_IRQHandler         DMA1_Stream1_IRQHandler
+#define USARTy_DMA_TX_IRQn               DMA1_Stream3_IRQn
+#define USARTy_DMA_RX_IRQn               DMA1_Stream1_IRQn
+#define USARTy_DMA_TX_IRQHandler         DMA1_Stream3_IRQHandler
+#define USARTy_DMA_RX_IRQHandler         DMA1_Stream1_IRQHandler
+
+/* Uncomment the line below if u want to us RS485 module */
+#define _RS485_
+
+#define RS485_DRIVER_PORT                GPIOA
+#define RS485_DRIVER_PIN                 GPIO_Pin_8
+#define RS485_DRIVER_CLOCK               RCC_APB2Periph_GPIOA
 
 
 
@@ -64,5 +73,9 @@ void USART_PutChar(uint8_t c);
 void USART_PutString(uint8_t *s);
 void USART_Config(void);
 void DMAx_Config(void);
+void RS485_Send_Enable(USART_TypeDef *USARTx);
+void RS485_Listen_Enable(USART_TypeDef *USARTx);
+void RS485_PutString(USART_TypeDef *USARTx, uint8_t *s);
+void RS485_PutChar(USART_TypeDef *USARTx, uint8_t c);
 
 #endif
